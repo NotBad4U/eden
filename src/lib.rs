704ff7eaeea1fa9059884bee959b3f6e4238fbb6
Alessio Coltellacci <lightplay8@gmail.com>
2018-02-05 00:18:46 +0100

    Remove networking and add collector and dispatcher
    I refactor the architecture because now we use zmq

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 extern crate slab;
 extern crate shred;
+extern crate zmq;
 
 pub mod agent;
 pub mod agent_system;
 pub mod agent_factory;
 pub mod packet;
 pub mod monitoring;
-pub mod networking;
 pub mod inbox;
+pub mod message_collector;
+pub mod dispatcher;
