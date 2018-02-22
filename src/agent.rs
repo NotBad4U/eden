2cf36302d8a14946d77fafed4d762f7d667f1b1d
Alessio Coltellacci <lightplay8@gmail.com>
2018-02-22 01:31:32 +0100

    Create AgentId custom type to increase visibility

--- src/agent.rs
+++ src/agent.rs
@@ -1,12 +1,14 @@
 use packet::Packet;
 use packet::Payload;
 
+pub type AgentId = usize;
+
 pub trait Agent {
     type P: Payload;
 
-    fn id(&self) -> usize;
+    fn id(&self) -> AgentId;
 
-    fn set_id(&mut self, id: usize);
+    fn set_id(&mut self, id: AgentId);
 
     fn is_dead(&self) -> bool;
 
